d8e9d94ab116eb52236412c9cfb5c92de5b88f9a
rgalarza <167392047+RGalarza77@users.noreply.github.com>
2024-12-04 01:21:29 -0300

    menu hamburguesa

---
+++ scriptMenu.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() { /*asegura de que el DOM esté completamente cargado antes de ejecutar el script.*/
+    const nav = document.querySelector("#nav");
+    const abrir = document.querySelector("#btn-abrir-menu");
+    const cerrar = document.querySelector("#btn-cerrar-menu");
+
+    abrir.addEventListener("click", () => {
+        nav.classList.add("visible"); // Agrego la clase visible al nav
+    });
+
+    cerrar.addEventListener("click", () => {
+        nav.classList.remove("visible"); // Remuevo la clase visible al nav
+    });
+});
